b0834332e2bed6590a054138654515a7fc42c9b1
ahmed3oun <72476024+ahmed3oun@users.noreply.github.com>
2021-08-25 20:54:12 +0100

    Update TestController.java

--- src/main/java/com/spring/security/postgresql/controllers/TestController.java
+++ src/main/java/com/spring/security/postgresql/controllers/TestController.java
@@ -16,16 +16,12 @@ public class TestController {
 	}
 	
 	@GetMapping("/user")
-	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
 	public String userAccess() {
 		return "User Content.";
 	}
 
-	@GetMapping("/mod")
-	@PreAuthorize("hasRole('MODERATOR')")
-	public String moderatorAccess() {
-		return "Moderator Board.";
-	}
+	
 
 	@GetMapping("/admin")
 	@PreAuthorize("hasRole('ADMIN')")
